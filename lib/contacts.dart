2827c058e4ca9301fc2e08febc73970cf0c526cc
Claire Wang <cywang0505@gmail.com>
2021-02-20 11:33:10 -0500

    Revert "Create and link to add contacts page"
    This reverts commit c8d3093cdd81f813eea62a61280a83896b087967.

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/material.dart';
-import 'add-contact.dart';
 
 void main() {
-  runApp(ContactsPage());
+  runApp(MyApp());
 }
 
-class ContactsPage extends StatefulWidget {
-  @override
-  _ContactsPageState createState() => _ContactsPageState();
-}
-
-class _ContactsPageState extends State<ContactsPage> {
-  int _selectedIndex = 0;
-  static List _pages = <Widget>[
-    AddContactForm(),
-    Text('index1'),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Contacts';
+    final title = 'Friends';
 
     return MaterialApp(
       title: title,
@@ -33,59 +15,52 @@ class _ContactsPageState extends State<ContactsPage> {
         appBar: AppBar(
           title: Text(title),
         ),
-        body: Center(
-          child: _pages.elementAt(_selectedIndex),
+        body: GridView.count(
+          // Create a grid with 2 columns. If you change the scrollDirection to
+          // horizontal, this produces 2 rows.
+          crossAxisCount: 2,
+          // Generate 100 widgets that display their index in the List.
+          children: <Widget>[
+            GridTile(
+              child: Icon(Icons.face),
+                footer: Text('Jennya',
+                  textAlign: TextAlign.center)
+            ),
+            GridTile(
+                child: Icon(Icons.face),
+                footer: Text('Claire',
+                    textAlign: TextAlign.center)
+            ),
+            GridTile(
+                child: Icon(Icons.face),
+                footer: Text('Vrushali',
+                    textAlign: TextAlign.center)
+            ),
+            GridTile(
+                child: Icon(Icons.face),
+                footer: Text('Nivashini',
+                    textAlign: TextAlign.center)
+            ),
+          ],
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add),
+              label: 'Add Friend',
+            ),
             BottomNavigationBarItem(
               icon: Icon(Icons.calendar_today),
               label: 'Calendar',
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: 'Add Friend',
+              icon: Icon(Icons.settings),
+              label: 'Settings',
             ),
           ],
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
+          // currentIndex: _selectedIndex,
+          // onTap: _onItemTapped,
         ),
-      ),
-    );
-  }
-}
-
-class ContactsGrid extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: GridView.count(
-        // Create a grid with 2 columns. If you change the scrollDirection to
-        // horizontal, this produces 2 rows.
-        crossAxisCount: 2,
-        // Generate 100 widgets that display their index in the List.
-        children: <Widget>[
-          GridTile(
-              child: Icon(Icons.face),
-              footer: Text('Jennya',
-                  textAlign: TextAlign.center)
-          ),
-          GridTile(
-              child: Icon(Icons.face),
-              footer: Text('Claire',
-                  textAlign: TextAlign.center)
-          ),
-          GridTile(
-              child: Icon(Icons.face),
-              footer: Text('Vrushali',
-                  textAlign: TextAlign.center)
-          ),
-          GridTile(
-              child: Icon(Icons.face),
-              footer: Text('Nivashini',
-                  textAlign: TextAlign.center)
-          ),
-        ],
       ),
     );
   }
